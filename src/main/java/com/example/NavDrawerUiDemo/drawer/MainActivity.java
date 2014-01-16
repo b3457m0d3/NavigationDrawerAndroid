0d0fcc24d314d4787cc8e8d476d7384f1e24f215
Edison Martinez <edison.martinez@me.com>
2014-01-15 22:45:01 -0500

    Complete Navigation Drawer Sample

--- src/main/java/com/example/NavDrawerUiDemo/drawer/MainActivity.java
+++ src/main/java/com/example/NavDrawerUiDemo/drawer/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.res.Configuration;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
@@ -14,30 +15,61 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import com.example.NavDrawerUiDemo.R;
-import com.example.NavDrawerUiDemo.drawer.fragment.IchiFragment;
-import com.example.NavDrawerUiDemo.drawer.fragment.NiFragment;
-import com.example.NavDrawerUiDemo.drawer.fragment.SanFragment;
-import com.example.NavDrawerUiDemo.drawer.fragment.ShiFragment;
+import com.example.NavDrawerUiDemo.drawer.adapter.NavDrawerListAdapter;
+import com.example.NavDrawerUiDemo.drawer.fragment.*;
+import com.example.NavDrawerUiDemo.drawer.model.NavDrawerItem;
+
+import java.util.ArrayList;
 
 /**
  * Created by edison on 1/14/14.
  */
 public class MainActivity extends Activity {
-    private String[] mPlanetTitles;
+    private String[] navMenuTitles;
+    private TypedArray navMenuIcons;
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
     private CharSequence mTitle;
     private CharSequence mDrawerTitle;
     private ActionBarDrawerToggle mDrawerToggle;
+    private ArrayList<NavDrawerItem> navDrawerItems;
+    private NavDrawerListAdapter adapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        mPlanetTitles = new String[]{"Ichi Fragment", "Ni Fragment", "San Fragment", "Shi Fragment"};
+        navMenuTitles = getResources().getStringArray(R.array.nav_drawer_items);
+        navMenuIcons = getResources()
+                .obtainTypedArray(R.array.nav_drawer_icons);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        navDrawerItems = new ArrayList<NavDrawerItem>();
+
+        // adding nav drawer items to array
+        // Home
+        navDrawerItems.add(new NavDrawerItem(navMenuTitles[0], navMenuIcons.getResourceId(0, -1)));
+        // Find People
+        navDrawerItems.add(new NavDrawerItem(navMenuTitles[1], navMenuIcons.getResourceId(1, -1)));
+        // Photos
+        navDrawerItems.add(new NavDrawerItem(navMenuTitles[2], navMenuIcons.getResourceId(2, -1)));
+        // Communities, Will add a counter here
+        navDrawerItems.add(new NavDrawerItem(navMenuTitles[3], navMenuIcons.getResourceId(3, -1), true, "22"));
+        // Pages
+        navDrawerItems.add(new NavDrawerItem(navMenuTitles[4], navMenuIcons.getResourceId(4, -1)));
+        // What's hot, We  will add a counter here
+        navDrawerItems.add(new NavDrawerItem(navMenuTitles[5], navMenuIcons.getResourceId(5, -1), true, "50+"));
+
+
+        // Recycle the typed array
+        navMenuIcons.recycle();
+
+        // setting the nav drawer list adapter
+        adapter = new NavDrawerListAdapter(getApplicationContext(),
+                navDrawerItems);
+        mDrawerList.setAdapter(adapter);
 
         mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
                 R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
@@ -58,8 +90,7 @@ public class MainActivity extends Activity {
         mDrawerLayout.setDrawerListener(mDrawerToggle);
 
         // Set the adapter for the list view
-        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
-                R.layout.drawer_list_item, mPlanetTitles));
+        //mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, navMenuTitles));
         // Set the list's click listener
         mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
 
@@ -110,17 +141,20 @@ public class MainActivity extends Activity {
         // Create a new fragment and specify the planet to show based on position
         Fragment fragment;
         switch (position) {
-            case 0:
+            case 1:
                 fragment = new IchiFragment();
                 break;
-            case 1:
+            case 2:
                 fragment = new NiFragment();
                 break;
-            case 2:
+            case 3:
                 fragment = new SanFragment();
                 break;
-            case 3:
+            case 4:
                 fragment = new ShiFragment();
+                break;
+            case 5:
+                fragment = new GoFragment();
                 break;
             default:
                 return;
@@ -138,7 +172,7 @@ public class MainActivity extends Activity {
 
         // Highlight the selected item, update the title, and close the drawer
         mDrawerList.setItemChecked(position, true);
-        setTitle(mPlanetTitles[position]);
+        setTitle(navMenuTitles[position]);
         mDrawerLayout.closeDrawer(mDrawerList);
     }
 
